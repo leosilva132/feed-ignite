c35a8939510018e2ef9ba1ec31c928095a692cb4
Leandro Viana <leandroviana.vs@gmail.com>
2022-06-15 15:10:41 -0300

    adicionado criação de comentários pelo text area

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -2,7 +2,12 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
-export function Comment(){
+
+interface CommentProps {
+    content: string;
+}
+
+export function Comment( {content}: CommentProps){
     return(
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://github.com/maykbrito.png"/>
@@ -19,7 +24,7 @@ export function Comment(){
                     </button>
                     </header>
 
-                    <p>Muito bom Leandro, parabéns!! 👏👏</p>
+                    <p>{content}</p>
                 </div>
                 <footer>
                     <button>
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,5 +1,6 @@
 import { format, formatDistanceToNow } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
+import { useState } from 'react';
 
 
 
@@ -15,7 +16,12 @@ interface PostProps {
 
 
 export function Post({author, content, publishAt}: PostProps){
-   
+    const [comments, setComments] = useState([
+        'Post muito bacana, hein!',
+    ])
+
+    const [newCommentText, setNewCommentText] = useState('')
+
     const publishedDateFormatted = format(publishAt, "d 'de' LLLL 'às' HH:mm'h'", {
         locale: ptBR,
     }
@@ -25,6 +31,17 @@ export function Post({author, content, publishAt}: PostProps){
         locale: ptBR,
         addSuffix: true,
     })
+
+    function handleCreateNewComment(){
+        event.preventDefault();
+
+        setComments([...comments, newCommentText]);
+        setNewCommentText('');
+    }
+
+    function handleNewCommentchange(){
+        setNewCommentText(event.target.value);
+    }
 
     return(
         <article className={styles.post}>
@@ -45,17 +62,22 @@ export function Post({author, content, publishAt}: PostProps){
             <div className={styles.content}>
               {content.map(line => {
                 if(line.type === 'paragraph') {
-                    return <p>{line.content}</p>
+                    return <p key={line.content}>{line.content}</p>
                 } else if (line.type === 'link') {
-                    return <p><a href="#">{line.content}</a></p>
+                    return <p key={line.content}><a href="#">{line.content}</a></p>
                 }
               })}
             </div>
 
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
 
-                <textarea placeholder='Deixe um comentário'/>
+                <textarea 
+                name='comment'
+                placeholder='Deixe um comentário'
+                value={newCommentText}
+                onChange={handleNewCommentchange}
+                />
 
                 <footer>
                     <button type='submit'>Publicar</button>
@@ -63,9 +85,9 @@ export function Post({author, content, publishAt}: PostProps){
             </form>
 
             <div className={styles.commentList}>
-                <Comment />
-                <Comment />
-                <Comment />
+                {comments.map(comment =>{
+                    return <Comment key={comment} content={comment}/>
+                })}
             </div>
         </article>
     );
